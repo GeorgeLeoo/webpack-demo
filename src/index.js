8be8469ec7bcfdee7344bc2e2ae5745afe00989c
GeorgeLeoo <georgeleeo@163.com>
2021-01-06 23:38:31 +0800

    加载 CSS
    为了在 JavaScript 模块中 import 一个 CSS 文件，你需要安装 style-loader 和 css-loader，并在 module 配置 中添加这些 loader：
    
    npm install --save-dev style-loader css-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import './style.css'
 
 function component() {
 	const element = document.createElement('div')
 
-  // - lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // + lodash，现在通过一个 script 引入
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  // lodash，现在通过一个 script 引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
 
 	return element
 }
