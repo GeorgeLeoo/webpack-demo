ffee3b107e633aede0e3417cdbaff6ddbcbcce20
GeorgeLeoo <georgeleeo@163.com>
2021-01-07 00:06:36 +0800

    自定义 JSON 模块 parser
    通过使用 自定义 parser 替代特定的 webpack loader，可以将任何 toml、yaml 或 json5 文件作为 JSON 模块导入。
    
    假设你在 src 文件夹下有一个 data.toml、一个 data.yaml 以及一个 data.json5 文件
    
    首先安装 toml，yamljs 和 json5 的 packages：
    
    npm install toml yamljs json5 --save-dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js',
@@ -31,6 +34,28 @@ module.exports = {
       {
         test: /\.xml$/i,
         use: ['xml-loader'],
+      },
+      // 自定义 JSON 模块 parser 
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
       },
     ]
   }
