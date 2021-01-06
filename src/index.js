6f4bc6aa556ac6135d03167906d70b0ae5ce456e
GeorgeLeoo <georgeleeo@163.com>
2021-01-07 00:16:12 +0800

    bundle

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import _ from 'lodash';
 function component() {
 	const element = document.createElement('div')
 
-  // - lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // + lodash，现在通过一个 script 引入
+  // lodash，现在通过一个 script 引入
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
 	return element
