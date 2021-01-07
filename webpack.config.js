be84be850f4dc5e3816f96abe5970c87cf4df888
GeorgeLeoo <georgeleeo@163.com>
2021-01-07 22:07:40 +0800

    防止重复(prevent duplication)
    入口依赖
    配置 dependOn option 选项，这样可以在多个 chunk 之间共享模块

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,23 @@ const path = require('path')
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared'
+    },
+    indanotherex: {
+      import: './src/another-module.js',
+      dependOn: 'shared'
+    },
+    shared: 'lodash'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // 如果我们要在一个 HTML 页面上使用多个入口时，
+  // 还需设置 optimization.runtimeChunk: 'single'
+  optimization: {
+    runtimeChunk: 'single'
   }
 }
