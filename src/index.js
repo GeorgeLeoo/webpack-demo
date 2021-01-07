d33feff4c00c580c1fcd8b02dc65067461421469
GeorgeLeoo <georgeleeo@163.com>
2021-01-07 22:16:42 +0800

    动态导入(dynamic import)
    当涉及到动态代码拆分时，webpack 提供了两个类似的技术。第一种，也是推荐选择的方式是，使用符合 ECMAScript 提案 的 import() 语法 来实现动态导入。第二种，则是 webpack 的遗留功能，使用 webpack 特定的 require.ensure。

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import _ from 'lodash';
-
-function component() {
-	const element = document.createElement('div')
-
-  // - lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // + lodash，现在通过一个 script 引入
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-	return element
+function getComponent() {
+	return import('lodash')
+		.then(({ default: _ }) => {
+			const element = document.createElement('div')
+			element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+			return element
+		})
+		.catch((error) => 'An error occurred while loading the component')
 }
 
-document.body.appendChild(component())
+getComponent().then(component => {
+  document.body.appendChild(component)
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,9 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    another: './src/another-module.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  // SplitChunksPlugin 插件可以将公共的依赖模块提取到已有的入口 chunk 中，
-  // 或者提取到一个新生成的 chunk。让我们使用这个插件，
-  // 将之前的示例中重复的 lodash 模块去除：
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
   }
 }
